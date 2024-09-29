bdd31f9d01408b7f4b76674e5dfc1f9bc551e5cc
parsaM110 <pmomeni80@gmail.com>
2024-09-29 11:49:01 +0330

    initial commit (adding mvn dep)

---
+++ src/test/java/com/example/batchspring/BatchSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
